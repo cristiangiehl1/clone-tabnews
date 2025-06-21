24ece731a2279bfc5c1cab4f11f400f39a61f846
cristiangiehl1 <cristiangiehl@gmail.com>
2025-06-21 18:44:46 -0300

    feat: create `migrator` model and apply it to `/api/v1/migrations`

---
+++ src/models/migrator.ts
@@ -0,0 +1,30 @@
+import database from "@/infra/database";
+import { runner, type RunnerOption } from "node-pg-migrate";
+import { resolve } from "node:path";
+
+async function runMigrations({ dryRun = true }: { dryRun?: boolean }) {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const runnerOptions: RunnerOption = {
+      dir: resolve(process.cwd(), "dist", "infra", "migrations"),
+      direction: "up",
+      migrationsTable: "pgmigrations",
+      verbose: true,
+      dryRun,
+      dbClient,
+    };
+
+    return await runner(runnerOptions);
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  runMigrations,
+};
+
+export default migrator;
